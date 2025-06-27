1974ab4b554e153e66f40ced1564aa1d3ce77f17
Furqat Teshaev <furqattrade@gmail.com>
2025-06-27 17:46:49 +0500

    fix docker issue

--- apps/dmr-server/src/mocks/handlers/centops.response.ts
+++ apps/dmr-server/src/mocks/handlers/centops.response.ts
@@ -23,4 +23,50 @@ export const handlers = [
       ],
     });
   }),
+
+  http.get('http://rabbitmq:15672/api/queues/%2F/:agentId', ({ params }) => {
+    const { agentId } = params;
+
+    return HttpResponse.json({
+      name: agentId,
+      vhost: '/',
+      durable: true,
+      auto_delete: false,
+      exclusive: false,
+      arguments: {},
+      node: 'rabbit@dmr-rabbitmq',
+      state: 'running',
+      message_stats: {
+        publish: 0,
+        publish_details: { rate: 0 },
+        deliver_get: 0,
+        deliver_get_details: { rate: 0 },
+      },
+      messages: 0,
+      messages_ready: 0,
+      messages_unacknowledged: 0,
+    });
+  }),
+
+  http.get('http://rabbitmq:15672/api/queues/%2F/:agentId.dlq', ({ params }) => {
+    const { agentId } = params;
+
+    return HttpResponse.json({
+      name: `${String(agentId)}.dlq`,
+      vhost: '/',
+      durable: true,
+      auto_delete: false,
+      exclusive: false,
+      arguments: {},
+      node: 'rabbit@dmr-rabbitmq',
+      state: 'running',
+      message_stats: {
+        publish: 0,
+        publish_details: { rate: 0 },
+      },
+      messages: 0,
+      messages_ready: 0,
+      messages_unacknowledged: 0,
+    });
+  }),
 ];
